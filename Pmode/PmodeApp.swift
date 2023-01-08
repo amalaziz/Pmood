6fa059dce6c2259959f06cfa1ee6fb699ed545ff
amalaziz <aaalothman45@sm.imamu.edu.sa>
2023-01-08 16:30:19 +0300

    Initial Commit

---
+++ Pmode/PmodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PmodeApp.swift
+//  Pmode
+//
+//  Created by ama alothman on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct PmodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
